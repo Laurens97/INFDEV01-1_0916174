4a35ce9870745e6b7f37895ef69c4a5b3ba6e34e
Jeroen Meijer <sonic.ex@hotmail.com>
2015-09-30 12:18:30 +0200

    Made new 5Ex1 with understandable code and working.

--- Assignment5Ex1/Assignment5Ex1/Assignment5Ex1.py
+++ Assignment5Ex1/Assignment5Ex1/Assignment5Ex1.py
@@ -1,3 +1,24 @@
-﻿txt=raw_input("Please input text to reverse: ") #input text
-newtxt=txt[::-1]    #reversing string by extended slice
+﻿firsttxt=raw_input("Please input text to reverse: ") #input text
+#newtxt=txt[::-1]    #reversing string by extended slice
+txt=list(firsttxt)
+x=len(txt)
+txt2=list(firsttxt)
+
+"""
+#TESTING#
+print "txt"
+print txt
+print "x "+str(x)
+print "txt2"
+print txt2
+#TESTING#
+"""
+
+while not x==0:
+    y=x*(-1)
+    z=x-1
+    txt2[z] = txt[y]
+    x-=1
+    
+newtxt="".join(txt2)
 print(newtxt)       #print result
